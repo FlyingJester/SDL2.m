e5de8c46aba1f52b42d461ced3a2b76318e42c7b
FlyingJester <foolkingcrown@gmail.com>
2015-08-13 11:58:21 -0800

    Added mouse handling

--- tower_event.m
+++ tower_event.m
@@ -7,13 +7,13 @@
 
 :- import_module io.
 :- import_module tower_rect.
-
-:- type button ---> left ; middle ; right.
+:- import_module tower_mouse.
 
 :- type tower_event ---> other_event
   ; time_event
   ; quit_event
   ; button_release_event(point, button)
+  ; mouse_motion_event(point, size)
   ; button_press_event(point, button).
 
 :- pred get_event(tower_event::uo, io::di, io::uo) is det.
@@ -25,11 +25,6 @@
 :- import_module bool.
 
 :- pragma foreign_decl("C", "#include <SDL2/SDL.h>").
-
-:- pragma foreign_enum("C", button/0,
-    [left - "SDL_BUTTON_LEFT", 
-     middle - "SDL_BUTTON_MIDDLE",
-     right - "SDL_BUTTON_RIGHT"]).
 
 %===============================================================================
 %=== Functions for use to wrap events from C 
@@ -51,8 +46,12 @@ create_button_press_event(X, Y, Button) = (button_press_event(point(X, Y), Butto
 :- pragma foreign_export("C", create_button_press_event(in, in, in) = (out), "createButtonPressEvent").
 
 :- func create_button_release_event(int::in, int::in, button::in) = (tower_event::out) is det.
-create_button_release_event(X, Y, Button) = (button_press_event(point(X, Y), Button)).
+create_button_release_event(X, Y, Button) = (button_release_event(point(X, Y), Button)).
 :- pragma foreign_export("C", create_button_release_event(in, in, in) = (out), "createButtonReleaseEvent").
+
+:- func create_mouse_motion_event(int::in, int::in, int::in, int::in) = (tower_event::out) is det.
+create_mouse_motion_event(X, Y, DX, DY) = (mouse_motion_event(point(X, Y), size(DX, DY))).
+:- pragma foreign_export("C", create_mouse_motion_event(in, in, in, in) = (out), "createMouseMotionEvent").
 
 %===============================================================================
 %=== SDL2 Event Bridge
@@ -69,19 +68,22 @@ create_button_release_event(X, Y, Button) = (button_press_event(point(X, Y), But
         }
         else if(e.type==SDL_MOUSEBUTTONDOWN){
             if(e.button.button!=SDL_BUTTON_LEFT && 
-                e.button.button!=SDL_BUTTON_LEFT && 
-                e.button.button!=SDL_BUTTON_LEFT)
+                e.button.button!=SDL_BUTTON_MIDDLE && 
+                e.button.button!=SDL_BUTTON_RIGHT)
                 Event = createOtherEvent();
             else
                 Event = createButtonPressEvent(e.button.x, e.button.y, e.button.button);
         }
         else if(e.type==SDL_MOUSEBUTTONUP){
             if(e.button.button!=SDL_BUTTON_LEFT && 
-                e.button.button!=SDL_BUTTON_LEFT && 
-                e.button.button!=SDL_BUTTON_LEFT)
+                e.button.button!=SDL_BUTTON_MIDDLE && 
+                e.button.button!=SDL_BUTTON_RIGHT)
                 Event = createOtherEvent();
             else
                 Event = createButtonReleaseEvent(e.button.x, e.button.y, e.button.button);
+        }
+        else if(e.type==SDL_MOUSEMOTION){
+            Event = createMouseMotionEvent(e.motion.x, e.motion.y, e.motion.xrel, e.motion.yrel);
         }
         else
             Event = createOtherEvent();
